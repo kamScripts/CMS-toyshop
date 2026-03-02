42fa97d5bcb11f1ab22db5ec9d5fa4b80faaf937
kamScripts <k.gebski1@gmail.com>
2026-03-02 18:17:01 +0000

    Create DB schema, create db create queries

---
+++ db_setup.sql
@@ -0,0 +1,58 @@
+CREATE TABLE brand (
+  brand_id INT AUTO_INCREMENT,
+  brand_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (brand_id)
+);
+
+CREATE TABLE scale (
+  scale_id INT AUTO_INCREMENT,
+  scale_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (scale_id)
+);
+
+CREATE TABLE collection (
+  collection_id INT AUTO_INCREMENT,
+  category_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (collection_id)
+);
+
+CREATE TABLE model (
+  model_id INT AUTO_INCREMENT,
+  model_name VARCHAR(50),
+  collection_id INT,
+  brand_id INT,
+  scale_id INT,
+  description TEXT,
+  PRIMARY KEY (model_id),
+  FOREIGN KEY (brand_id)
+      REFERENCES brand(brand_id)
+      ON DELETE CASCADE,
+  FOREIGN KEY (scale_id)
+      REFERENCES scale(scale_id),
+  FOREIGN KEY (collection_id)
+      REFERENCES collection(collection_id)
+      ON DELETE CASCADE
+);
+
+CREATE TABLE user (
+  user_id INT AUTO_INCREMENT,
+  username VARCHAR(16) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (user_id)
+);
+
+CREATE TABLE variant (
+  variant_id INT AUTO_INCREMENT,
+  model_id INT,
+  variant VARCHAR(20),
+  sku VARCHAR(12),
+  price DECIMAL(5,2),
+  stock INT,
+  imagepath VARCHAR(100),
+  is_featured TINYINT(1),
+  PRIMARY KEY (variant_id),
+  FOREIGN KEY (model_id)
+      REFERENCES model(model_id)
+      ON DELETE CASCADE
+);
